e742ff6ae01f46a5a036585f9e8f96c51ebfc604
Filiberto <filiberto.dev@gmail.com>
2021-12-07 15:47:34 +0100

    improved insertion in the matrix with custom Y

--- tetris.h
+++ tetris.h
@@ -42,22 +42,23 @@ void deleteTetris(Tetris *tetris)
     free(tetris);
 }
 
-/* 
+/*
     \brief check if 2 matrix are intersected each other
  */
-int isIntersected(Matrix* tetrisMatrix, Tetramino *t, int x, int y){
+int isIntersected(Matrix *tetrisMatrix, Tetramino *t, int x, int y)
+{
     int startXTetramino = offsetX(t);
     int startYTetramino = offsetY(t);
 
-    //printf("\nh: %d\nw: %d\nstart tetramino x: %d\nstart tetramino y: %d\nconsider this --> \n", t->heigth, t->width, startXTetramino, startYTetramino);
+    //printw("\nh: %d\nw: %d\nstart tetramino x: %d\nstart tetramino y: %d\nconsider this --> \n", t->heigth, t->width, startXTetramino, startYTetramino);
 
     for (int i = 0; i < t->heigth; i++)
     {
         for (int j = 0; j < t->width; j++)
         {
-            //printf("%d ", i * t->matrix.cols + j);
-            //printf("%d", t->matrix.map[(i+startYTetramino) * t->matrix.cols + (j + startXTetramino)]);
-            //printf("%d", tetrisMatrix->map[(y+i) * tetrisMatrix->cols + (x+j)]);
+            // printf("%d ", i * t->matrix.cols + j);
+            // printf("%d", t->matrix.map[(i+startYTetramino) * t->matrix.cols + (j + startXTetramino)]);
+            // printf("%d", tetrisMatrix->map[(y+i) * tetrisMatrix->cols + (x+j)]);
 
             if (t->matrix.map[(i + startYTetramino) * t->matrix.cols + (j + startXTetramino)] == 1 && tetrisMatrix->map[(y + i) * tetrisMatrix->cols + (x + j)] == 1)
                 return 1;
@@ -67,35 +68,46 @@ int isIntersected(Matrix* tetrisMatrix, Tetramino *t, int x, int y){
     return 0;
 }
 
-int findY(Matrix *tetrisMatrix, Tetramino *t, int x){
-    int y, count=0;
+int findY(Matrix *tetrisMatrix, Tetramino *t, int x)
+{
+    int y, count = 0;
 
-    for(y=0; y<tetrisMatrix->rows-t->heigth+1; y++){
-        //printf("[%d]\n", y);
-        if(isIntersected(tetrisMatrix, t, x, y)){
-            return y-1;
-        } else  
+    for (y = 0; y < tetrisMatrix->rows - t->heigth + 1; y++)
+    {
+        //printw("[%d]\n", y);
+        if (isIntersected(tetrisMatrix, t, x, y))
+        {
+            return y - 1;
+        }
+        else
             count++;
     }
-    return count-1;
+    return count - 1;
 }
 
-void insertTetramino(Matrix *tetrisMatrix, Tetramino *t, int x){
+int insertTetramino(Matrix *tetrisMatrix, Tetramino *t, int x, int y, int gravity)
+{
     if (x > DEFAULT_WIDTH)
-        EXIT_FAILURE; // impossible to insert over the width
+        return 0; // impossible to insert over the width
 
-    int offX = offsetX(t), offY = offsetY(t), y=0;
+    int offX = offsetX(t), offY = offsetY(t);
 
-    printf("size    W:%d\n\tH: %d\noffset  X: %d\n\tY: %d\n", t->width,t->heigth,offX, offY);
+    //printf("size    W:%d\n\tH: %d\noffset  X: %d\n\tY: %d\n", t->width, t->heigth, offX, offY);
 
     if (x + t->width <= DEFAULT_WIDTH)
     {
-        //printf("intersected: %d\n", isIntersected(tetrisMatrix, t, x, 8));
-        //printf("\n-->y: %d <--\n", findY(tetrisMatrix, t, x));
+        // printf("intersected: %d\n", isIntersected(tetrisMatrix, t, x, 8));
+        //printw("\n-->y: %d <--\n", findY(tetrisMatrix, t, x));
 
-        y = findY(tetrisMatrix, t, x);
-            // its possible
-            for (int row = offY; row < t->matrix.rows; row++)
+        if(gravity) //if set the tetramine will go to the bottom of the main matrix
+            y = findY(tetrisMatrix, t, x); //y will be overwritten
+        else{
+            if (isIntersected(tetrisMatrix, t, x, y))
+                return 0;
+        }
+        // its possible
+        if(y < 0) return 0;
+        for (int row = offY; row < t->matrix.rows; row++)
         {
             for (int col = offX; col < t->matrix.cols; col++)
             {
@@ -105,5 +117,7 @@ void insertTetramino(Matrix *tetrisMatrix, Tetramino *t, int x){
                 }
             }
         }
+        return 1; //inserted
     }
+    return 0; //not inserted
 }
