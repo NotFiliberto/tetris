3084483a6e18ee5d7cb3d0a7c6633f6ab05ad54e
Filiberto <filiberto.dev@gmail.com>
2021-12-13 19:34:15 +0100

    setup for rotate matrix

--- src/include/mylibs/gamescreen.h
+++ src/include/mylibs/gamescreen.h
@@ -1,22 +1,28 @@
-int manageTetraminoXInput(Tetramino* t, int x, char input)
+int tetraminoXMoving(Tetramino *t, int x, char input)
 {
-    if(input == 'A' || input == 'D'){
+    if (input == 'A' || input == 'D')
+    {
         int op = (input == 'A') ? -1 : 1;
         if (input == 'A' && (x - 1 > -1))
-            return x + op;
+            x += op;
         if (input == 'D' && (x + t->width < DEFAULT_WIDTH))
-            return x + op;
+            x += op;
+    }
+    if(input == '\n'){
+        if ((x + t->width > DEFAULT_WIDTH))
+            x = DEFAULT_WIDTH-t->width;
     }
 
     return x;
 }
 
-void printMatrixW(Matrix *matrix, int rspace)
+void printMatrixW(Matrix *matrix, int rspace, int offsetX, int offsetY)
 {
-    for (int row = 0; row < matrix->rows; row++)
+    for (int row = offsetY; row < matrix->rows; row++)
     {
-        for (int i = 0; i < rspace; i++) printw(" "); //space for better visualization 
-        for (int col = 0; col < matrix->cols; col++)
+        for (int i = 0; i < rspace; i++)
+            printw(" "); // space for better visualization
+        for (int col = offsetX; col < matrix->cols; col++)
         {
             printw("%d", matrix->map[col + row * matrix->cols]);
         }
--- src/include/mylibs/tetris.h
+++ src/include/mylibs/tetris.h
@@ -4,9 +4,13 @@
 typedef struct tetris
 {
     Matrix *matrix;
+    Tetramino* tetramino; // current tetramino selected, with all of his options
+    int tetraminoType;
+    int lastX; // last X position for the last tetramino
     // score
     // gamemode
     // ecc...
+    Tetramino t;
 } Tetris;
 
 int testingMap[] = { // 10x7
@@ -21,9 +25,39 @@ int testingMap[] = { // 10x7
     0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0};
 
-Tetris *createTetris(int cols, int rows)
+Tetramino *createTetramino(Tetramino *t)
+{
+    Tetramino *tetramino = (Tetramino *)malloc(sizeof(Tetramino));
+    tetramino->code = t->code;
+    tetramino->heigth = t->heigth;
+    tetramino->width = t->width;
+    tetramino->matrix = *createMatrix(t->matrix.cols, t->matrix.rows);
+
+    for (int i = 0; i < t->matrix.cols * t->matrix.rows; i++)
+    {
+        tetramino->matrix.map[i] = t->matrix.map[i];
+    }
+
+    tetramino->offsetX = offsetX(t);
+    tetramino->offsetY = offsetY(t);
+
+    return tetramino;
+}
+
+void deleteTetramino(Tetramino *t)
+{
+    free(t);
+}
+
+Tetris *
+createTetris(int cols, int rows)
 {
     Tetris *tetris = (Tetris *)malloc(sizeof(Tetris));
+    tetris->tetraminoType = 0;
+    tetris->lastX = 0;
+
+    tetris->tetramino = createTetramino(&tetramini[TETRAMINO_I]); // copy tetramino I from the array
+
     // tetris->matrix = createMatrix(rows, cols); // the fuction returns the pointer of the created matrix
     // clearMatrix(tetris->matrix);               // set up the effective map to all 0
 
@@ -50,7 +84,7 @@ int isIntersected(Matrix *tetrisMatrix, Tetramino *t, int x, int y)
     int startXTetramino = offsetX(t);
     int startYTetramino = offsetY(t);
 
-    //printw("\nh: %d\nw: %d\nstart tetramino x: %d\nstart tetramino y: %d\nconsider this --> \n", t->heigth, t->width, startXTetramino, startYTetramino);
+    // printw("\nh: %d\nw: %d\nstart tetramino x: %d\nstart tetramino y: %d\nconsider this --> \n", t->heigth, t->width, startXTetramino, startYTetramino);
 
     for (int i = 0; i < t->heigth; i++)
     {
@@ -74,7 +108,7 @@ int checkYRowPiece(Matrix *tetrisMatrix, Tetramino *t, int x, int y)
     int counter = 0;
     for (int j = 0; j < t->width; j++)
     {
-        if (tetrisMatrix->map[tetrisMatrix->cols*y + x + j] == 1)
+        if (tetrisMatrix->map[tetrisMatrix->cols * y + x + j] == 1)
             counter++;
     }
 
@@ -87,12 +121,12 @@ int findY(Matrix *tetrisMatrix, Tetramino *t, int x)
 
     for (y = 0; y < tetrisMatrix->rows - t->heigth + 1; y++)
     {
-        //printw("[%d]\n", y);
-        if (checkYRowPiece(tetrisMatrix, t, x, y) > 0) //if the row in the matrix is not empty u cant insert the new Tetramino
+        // printw("[%d]\n", y);
+        if (checkYRowPiece(tetrisMatrix, t, x, y) > 0) // if the row in the matrix is not empty u cant insert the new Tetramino
             return y - 1;
         if (isIntersected(tetrisMatrix, t, x, y))
             return y - 1;
-        count++; //else increase counter
+        count++; // else increase counter
     }
     return count - 1;
 }
@@ -104,21 +138,23 @@ int insertTetramino(Matrix *tetrisMatrix, Tetramino *t, int x, int y, int gravit
 
     int offX = offsetX(t), offY = offsetY(t);
 
-    //printf("size    W:%d\n\tH: %d\noffset  X: %d\n\tY: %d\n", t->width, t->heigth, offX, offY);
+    // printf("size    W:%d\n\tH: %d\noffset  X: %d\n\tY: %d\n", t->width, t->heigth, offX, offY);
 
     if (x + t->width <= DEFAULT_WIDTH)
     {
         // printf("intersected: %d\n", isIntersected(tetrisMatrix, t, x, 8));
-        //printw("\n-->y: %d <--\n", findY(tetrisMatrix, t, x));
+        // printw("\n-->y: %d <--\n", findY(tetrisMatrix, t, x));
 
-        if(gravity) //if set the tetramine will go to the bottom of the main matrix
-            y = findY(tetrisMatrix, t, x); //y will be overwritten
-        else{
+        if (gravity)                       // if set the tetramine will go to the bottom of the main matrix
+            y = findY(tetrisMatrix, t, x); // y will be overwritten
+        else
+        {
             if (isIntersected(tetrisMatrix, t, x, y))
                 return 0;
         }
         // its possible
-        if(y < 0) return 0;
+        if (y < 0)
+            return 0;
         for (int row = offY; row < t->matrix.rows; row++)
         {
             for (int col = offX; col < t->matrix.cols; col++)
@@ -129,7 +165,7 @@ int insertTetramino(Matrix *tetrisMatrix, Tetramino *t, int x, int y, int gravit
                 }
             }
         }
-        return 1; //inserted
+        return 1; // inserted
     }
-    return 0; //not inserted
+    return 0; // not inserted
 }
